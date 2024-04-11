fb2bc3e4963c771cc96065435b1f135248dc7c9b
Askarbekov Aktan <akn392204@gmail.com>
2024-04-11 18:51:30 +0600

    Add OrderController.cs where there are Index and Create actions

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductShop.Models;
+
+namespace ProductShop.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly ProductContext _context;
+    
+    public OrderController(ProductContext context)
+    {
+        _context = context;
+    }
+    
+    public IActionResult Index()
+    {
+        List<Order> orders = _context.Orders.Include(o => o.Product).ToList();
+        return View(orders);
+    }
+    
+    public IActionResult Create(int Id)
+    {
+        Product product = _context.Products.FirstOrDefault(p => p.Id == Id);
+        return View(new Order() {Product = product});
+    }
+
+    [HttpPost]
+    public IActionResult Create(Order order)
+    {
+        if (order != null)
+        {
+            _context.Add(order);
+            _context.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
+}
